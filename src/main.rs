36bfd275521ed1d966a169db19f962f6787efcf5
Krahos <krahos@outlook.com>
2019-11-20 16:41:47 +0100

    Migrated to async-std and rustls

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use std::thread;
 
 fn main() {
     // Loading the server certificate and private key.
-    let configuration = load_config().unwrap();
+    let configuration = load_config().expect("Failed to load server configuration.");
 
     // Listening for incoming connections in another thread.
     match thread::Builder::new()
@@ -34,31 +34,35 @@ fn main() {
 
 /// Loads server configuration in terms of certificate, key, settings. TODO: better error handling.
 fn load_config() -> io::Result<ServerConfig> {
-    // Read certificates.
-    let certificate_path = Path::new("./Certificate");
-    let mut buffer = BufReader::new(File::open(certificate_path).unwrap());
-
-    let certificates = pemfile::certs(&mut buffer).unwrap();
-
     // Read key.
-    let key_path = Path::new("./key");
-    let mut buffer = BufReader::new(File::open(key_path).unwrap());
+    let key_path = Path::new("end.rsa");
+    let mut buffer = BufReader::new(File::open(key_path).expect("Unable to open key.pem."));
 
-    let mut private_key = pemfile::rsa_private_keys(&mut buffer).unwrap();
+    let mut private_key =
+        pemfile::rsa_private_keys(&mut buffer).expect("Unable to read private key.");
+
+    // Read certificates.
+    let certificate_path = Path::new("end.cert");
+    let mut buffer =
+        BufReader::new(File::open(certificate_path).expect("Unable to open identity.pfx."));
+
+    let certificates = pemfile::certs(&mut buffer).expect("Unable to open identity certificate.");
 
     // Create server configuration.
     let mut configuration = ServerConfig::new(NoClientAuth::new());
 
     configuration
         .set_single_cert(certificates, private_key.remove(0))
-        .unwrap();
+        .expect("Failed to set single certificate.");
 
     Ok(configuration)
 }
 
 async fn start_listening(configuration: ServerConfig) {
     // Creating TCP listener.
-    let listener = TcpListener::bind("127.0.0.1:5568").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:5568")
+        .await
+        .expect("Unable to bind to localhost.");
 
     // Creating TLS acceptor.
     let tls_acceptor = TlsAcceptor::from(Arc::new(configuration));
@@ -82,12 +86,18 @@ async fn start_listening(configuration: ServerConfig) {
 
 /// Serves the client requests.
 async fn handle_client(tls_acceptor: TlsAcceptor, connection: (TcpStream, SocketAddr)) {
-    let mut stream = tls_acceptor.accept(connection.0).await.unwrap();
+    let mut stream = tls_acceptor
+        .accept(connection.0)
+        .await
+        .expect("Unable to accept TLS connection.");
     println!("Accepted TcpConnection with {:?}.", connection.1);
     loop {
         // Read request. TODO: read request as flatbuffer.
         let mut buffer = [0; 20];
-        stream.read(&mut buffer).await.unwrap();
+        stream
+            .read(&mut buffer)
+            .await
+            .expect("Unable to read from stream.");
         let request = String::from_utf8_lossy(&buffer[..]);
         let request = request.trim_end_matches(char::from(0));
 
@@ -96,20 +106,29 @@ async fn handle_client(tls_acceptor: TlsAcceptor, connection: (TcpStream, Socket
             "req1" => {
                 // Answer to req1.
                 println!("Received: {:?}", request);
-                stream.write_all("ans1".as_bytes()).await.unwrap();
-                stream.flush().await.unwrap();
+                stream
+                    .write_all("ans1".as_bytes())
+                    .await
+                    .expect("Unable to write on stream.");
+                stream.flush().await.expect("Unable to flush stream.");
             }
 
             "enqueue" => {
-                stream.write_all("Queued".as_bytes()).await.unwrap();
-                stream.flush().await.unwrap();
+                stream
+                    .write_all("Queued".as_bytes())
+                    .await
+                    .expect("Unable to write on stream.");
+                stream.flush().await.expect("Unable to flush stream.");
             }
 
             "close" => {
                 // Terminate connection.
                 println!("Received: {:?}", request);
-                stream.write_all("Terminating".as_bytes()).await.unwrap();
-                stream.flush().await.unwrap();
+                stream
+                    .write_all("Terminating".as_bytes())
+                    .await
+                    .expect("Unable to write on stream.");
+                stream.flush().await.expect("Unable to flush stream.");
 
                 break;
             }
@@ -117,8 +136,11 @@ async fn handle_client(tls_acceptor: TlsAcceptor, connection: (TcpStream, Socket
             _ => {
                 // Default case. TODO: handle bad actors.
                 println!("{:?} was not a valid request.", request);
-                stream.write_all("Nope".as_bytes()).await.unwrap();
-                stream.flush().await.unwrap();
+                stream
+                    .write_all("Nope".as_bytes())
+                    .await
+                    .expect("Unable to write on stream.");
+                stream.flush().await.expect("Unable to flush stream.");
             }
         }
     }
@@ -134,7 +156,10 @@ async fn handle_user_input() {
     loop {
         println!("Awaiting input:");
         buffer.clear();
-        stdin.read_line(&mut buffer).await.unwrap();
+        stdin
+            .read_line(&mut buffer)
+            .await
+            .expect("Unable to read from stdin.");
 
         match buffer.trim() {
             "--exit" => {
